116693520b481a7e2fc59b58b997fb469274964d
miles <a09112332@gmail.com>
2025-09-22 12:48:01 +0800

    chore: npm run format

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,6 +1,6 @@
 const WS_URL = {
   UPDATE_PRICE: 'wss://ws.btse.com/ws/oss/futures',
-  LAST_PRICE: 'wss://ws.btse.com/ws/futures'
+  LAST_PRICE: 'wss://ws.btse.com/ws/futures',
 }
 
 export { WS_URL }
